ddaf886f3c66fd456b2c7946ee78b43c7faa1b7b
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-03-01 19:11:45 +0100

    Repository realizado

--- src/main/java/es/fplumara/dam1/campeonato/repository/DeportistaRepositoryImpl.java
+++ src/main/java/es/fplumara/dam1/campeonato/repository/DeportistaRepositoryImpl.java
@@ -1,4 +1,40 @@
 package es.fplumara.dam1.campeonato.repository;
 
-public class DeportistaRepositoryImpl {
+import es.fplumara.dam1.campeonato.model.Deportista;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class DeportistaRepositoryImpl implements DeportistaRepository{
+    private Map<String,Deportista> datos;
+
+    public DeportistaRepositoryImpl(Map<String, Deportista> datos) {
+        this.datos = datos;
+    }
+
+    @Override
+    public void save(Deportista d) {
+        datos.put(d.getId(), d);
+    }
+
+    @Override
+    public Optional<Deportista> findById(String id) {
+        return Optional.ofNullable(datos.get(id));
+    }
+
+    @Override
+    public List<Deportista> findByPais(String pais) {
+
+        return datos.values()
+                .stream()
+                .filter(deportista -> deportista.getPais().equals(pais))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Deportista> listAll() {
+        return datos.values().stream().toList();
+    }
 }
--- src/main/java/es/fplumara/dam1/campeonato/repository/ResultadoRepositoryImpl.java
+++ src/main/java/es/fplumara/dam1/campeonato/repository/ResultadoRepositoryImpl.java
@@ -1,4 +1,35 @@
 package es.fplumara.dam1.campeonato.repository;
 
-public class ResultadoRepositoryImpl {
+import es.fplumara.dam1.campeonato.model.Resultado;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class ResultadoRepositoryImpl implements ResultadoRepository{
+    private Map<String,Resultado> datos;
+
+    public ResultadoRepositoryImpl(Map<String, Resultado> datos) {
+        this.datos = datos;
+    }
+
+    @Override
+    public void save(Resultado r) {
+        datos.put(r.getId(),r);
+    }
+
+    @Override
+    public Optional<Resultado> findById(String id) {
+        return Optional.ofNullable(datos.get(id));
+    }
+
+    @Override
+    public List<Resultado> listAll() {
+        return datos.values().stream().toList();
+    }
+
+    @Override
+    public boolean existByPruebaYDeportista(String idPrueba, String idDeportista) {
+        return false;
+    }
 }
