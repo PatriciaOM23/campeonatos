5bd765c94f61ac2b7bfabc622b24b74289bfd111
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-03-01 18:42:01 +0100

    Model realizado

--- src/main/java/es/fplumara/dam1/campeonato/model/Resultado.java
+++ src/main/java/es/fplumara/dam1/campeonato/model/Resultado.java
@@ -1,4 +1,31 @@
 package es.fplumara.dam1.campeonato.model;
 
+import java.util.stream.Stream;
+
 public class Resultado {
+    private String id;
+    private String idPrueba;
+    private TipoPrueba tipoPrueba;
+    private String idDeportista;
+    private int posicion;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getIdPrueba() {
+        return idPrueba;
+    }
+
+    public TipoPrueba getTipoPrueba() {
+        return tipoPrueba;
+    }
+
+    public String getIdDeportista() {
+        return idDeportista;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
 }
